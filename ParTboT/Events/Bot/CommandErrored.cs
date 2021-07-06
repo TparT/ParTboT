c7e6c3a3927b17ac75c331ee6edf4e54f27d3fbc
TparT <66745515+TparT@users.noreply.github.com>
2021-07-06 18:23:55 +0300

    Changed falling 'if' statements to 'switch-case' statements

--- ParTboT/Events/Bot/CommandErrored.cs
+++ ParTboT/Events/Bot/CommandErrored.cs
@@ -26,7 +26,6 @@ namespace ParTboT.Events.Bot
 
         public async Task Command_Errored(CommandsNextExtension sender, CommandErrorEventArgs e)
         {
-
             // let's log the error details
             //e.Context.Client.Logger.LogError(BotEventId, $"{e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? $"{e.Context.Message.Content} (unknown command)"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"}", DateTime.Now);
             bool SendEmbed = false;
@@ -44,77 +43,72 @@ namespace ParTboT.Events.Bot
             DiscordEmbedBuilder embed = new();
             embed.Color = DiscordColor.Red;
 
-            if (e.Exception is InvalidOverloadException IOE)
+            switch (e.Exception)
             {
-                IOE.OutputBigExceptionError();
-            }
-            // let's check if the error is a result of lack
-            // of required permissions
-            else if (e.Exception is ChecksFailedException ex)
-            {
-                SendEmbed = true;
-                foreach (var FailedCheck in ex.FailedChecks)
-                {
-                    if (FailedCheck is RequireBotPermissionsAttribute RequiredBotPermissions)
+                case InvalidOverloadException IOE:
+                    IOE.OutputBigExceptionError();
+                    break;
+                case ChecksFailedException ex:
                     {
-                        embed.Title = $"{emoji} {ex.Context.Client.CurrentUser.Username} doesn't have permissions";
-                        embed.Description = $"{ex.Context.Client.CurrentUser.Username} must have the '{RequiredBotPermissions.Permissions.ToPermissionString().Replace("guild", "Server")}' in order to run this command.";
-                        //await ex.Context.RespondAsync(embed).ConfigureAwait(false);
-                    }
-                    else if (FailedCheck is RequireUserPermissionsAttribute RequiredUserPermissions)
-                    {
-                        embed.Title = $"{emoji} Access denied";
-                        embed.Description = $"You do not have the {RequiredUserPermissions.Permissions.ToPermissionString().Replace("guild", "Server")}, which is required to use this command.";
-                        //await ex.Context.RespondAsync(embed).ConfigureAwait(false);
-                    }
-                    else if (FailedCheck is RequirePermissionsAttribute RequirePermissions)
-                    {
-                        embed.Title = $"{emoji} Access denied";
-                        embed.Description = $"{ex.Context.Client.CurrentUser.Username} AND YOU do not have the {RequirePermissions.Permissions.ToPermissionString().Replace("guild", "Server")}, which is required to use this command.";
-                        //await ex.Context.RespondAsync(embed).ConfigureAwait(false);
-                    }
-                    else if (FailedCheck is RequireGuildAttribute RequiredGuild)
-                    {
-                        embed.Title = $"{emoji} Access denied";
-                        embed.Description = $"This command can only be executed in a server!";
-                    }
-                }
-            }
-            else if (e.Exception is CommandNotFoundException CNFE)
-            {
-                //await e.Context.RespondAsync($"```css\n[ {e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? $"{e.Context.Message.Content} (unknown command)"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"} {DateTime.Now} ]\n```").ConfigureAwait(false);
+                        SendEmbed = true;
+                        foreach (var FailedCheck in ex.FailedChecks)
+                        {
+                            switch (FailedCheck)
+                            {
+                                case RequireBotPermissionsAttribute RequiredBotPermissions:
+                                    embed.Title = $"{emoji} {ex.Context.Client.CurrentUser.Username} doesn't have permissions";
+                                    embed.Description = $"{ex.Context.Client.CurrentUser.Username} must have the '{RequiredBotPermissions.Permissions.ToPermissionString().Replace("guild", "Server")}' in order to run this command.";
+                                    break;
+                                case RequireUserPermissionsAttribute RequiredUserPermissions:
+                                    embed.Title = $"{emoji} Access denied";
+                                    embed.Description = $"You do not have the {RequiredUserPermissions.Permissions.ToPermissionString().Replace("guild", "Server")}, which is required to use this command.";
+                                    break;
+                                case RequirePermissionsAttribute RequirePermissions:
+                                    embed.Title = $"{emoji} Access denied";
+                                    embed.Description = $"{ex.Context.Client.CurrentUser.Username} AND YOU do not have the {RequirePermissions.Permissions.ToPermissionString().Replace("guild", "Server")}, which is required to use this command.";
+                                    break;
+                                case RequireGuildAttribute RequiredGuild:
+                                    embed.Title = $"{emoji} Access denied";
+                                    embed.Description = $"This command can only be executed in a server!";
+                                    break;
+                            }
+                        }
 
-                //SpellingCorrectingService spelling = ;
+                        break;
+                    }
 
-                // A CUSTOM sentence
-                string CustomSentence_Correction = string.Empty;
-                foreach (string Word in e.Context.Message.Content.Split(' '))
-                    CustomSentence_Correction += _services.SpellingCorrecting.Correct(Word) + " ";
+                case CommandNotFoundException CNFE:
+                    {
+                        //await e.Context.RespondAsync($"```css\n[ {e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? $"{e.Context.Message.Content} (unknown command)"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"} {DateTime.Now} ]\n```").ConfigureAwait(false);
 
-                await e.Context.RespondAsync(($"Command {CNFE.CommandName} not found, did you mean: `{ CustomSentence_Correction }` ?").ToString()).ConfigureAwait(false);
-            }
-            else if (e.Exception is RateLimitException RLE)
-            {
-                Console.WriteLine(RLE.JsonMessage);
-            }
-            else if (e.Exception is BadRequestException BRE)
-            {
-                Console.WriteLine($"Errors: {BRE.Errors}\n" +
-                                  $"Web response: {BRE.WebResponse.Response}");
-            }
-            else
-            {
-                e.Exception.OutputBigExceptionError();
-                DiscordActivity ErrorStatus = new DiscordActivity($"{(await e.Context.Client.GetCurrentApplicationAsync().ConfigureAwait(false)).Owners.FirstOrDefault().Username} getting mad about that an error has occured with the {e.Command.Name} command", ActivityType.Watching);
-                await e.Context.Client.UpdateStatusAsync(ErrorStatus).ConfigureAwait(false);
+                        string CustomSentence_Correction = string.Empty;
+                        foreach (string Word in e.Context.Message.Content.Split(' '))
+                            CustomSentence_Correction += _services.SpellingCorrecting.Correct(Word) + " ";
+
+                        await e.Context.RespondAsync($"Command {CNFE.CommandName} not found, did you mean: `{ CustomSentence_Correction }` ?".ToString()).ConfigureAwait(false);
+                        break;
+                    }
+
+                case RateLimitException RLE:
+                    Console.WriteLine(RLE.JsonMessage);
+                    break;
+                case BadRequestException BRE:
+                    Console.WriteLine($"Errors: {BRE.Errors}\n" +
+                                      $"Web response: {BRE.WebResponse.Response}");
+                    break;
+                default:
+                    {
+                        e.Exception.OutputBigExceptionError();
+                        DiscordActivity ErrorStatus = new DiscordActivity($"{(await e.Context.Client.GetCurrentApplicationAsync().ConfigureAwait(false)).Owners.FirstOrDefault().Username} getting mad about that an error has occured with the {e.Command.Name} command", ActivityType.Watching);
+                        await e.Context.Client.UpdateStatusAsync(ErrorStatus).ConfigureAwait(false);
+                        break;
+                    }
             }
 
             if (SendEmbed == true)
             {
                 await e.Context.RespondAsync(embed).ConfigureAwait(false);
             }
-
-
         }
     }
 }
